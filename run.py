78ad7db188cac2bc2bee01381afaf2b5bf6cf85e
Tabeth Nkangoh <benink@gmail.com>
2014-03-20 17:36:56 -0400

    init

--- run.py
+++ run.py
@@ -1,13 +1,17 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
 import twilio.twiml
 import valentine
 
+SECRET_KEY = 'pineapple'
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 #Foods
-breakfast = valentine.createMenu("Breakfast")
-lunch = valentine.createMenu("Lunch")
-dinner = valentine.createMenu("Dinner")
+food = {
+	"breakfast": valentine.createMenu('Breakfast'),
+	"lunch": valentine.createMenu('Lunch'),
+	"dinner": valentine.createMenu('Dinner'),
+}
 
 # Add our own number to this list!
 callers = {
@@ -19,14 +23,29 @@ callers = {
 def hello_monkey():
 	"""Respond to incoming calls..."""
 
+
+	counter = session.get('counter', 0)
+	counter += 1
+	session['counter'] = counter
+
+	#First message
 	from_number = request.values.get('From', None)
 	if from_number in callers:
-		message = callers[from_number] + ", thanks for the message! As you've requested, here's today's breakfast menu: "
-		message = message + str(breakfast)
+		message = callers[from_number] + ", thanks for the message! Do you want to know today's menu? Let me know! You can say: breakfast, lunch, dinner, or all for all of them"
+		message = message
 	else:
-		message = "Thanks for the message! As you've requested, here's today's breakfast menu: "
-		message = message + str(breakfast)
+		message = "Thanks for the message! Do you want to know today's menu? Let me know! You can say: breakfast, lunch, dinner, or all for all of them"
+		message = message 
 		
+
+	#Second message
+	if counter > 1:
+		message_body = request.values.get('Body', None)
+		if message_body in food:
+
+			message = "Here's the " + message_body + "menu: "
+		 	+ message_body[food]
+
 	resp = twilio.twiml.Response()
 	resp.message(message)
 
