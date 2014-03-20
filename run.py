66c80ff93598220ee642dbc7b3255c33a3cea528
Tabeth Nkangoh <benink@gmail.com>
2014-03-20 00:29:37 -0400

    init

--- run.py
+++ run.py
@@ -3,12 +3,24 @@ import twilio.twiml
 
 app = Flask(__name__)
 
+# Add our own number to this list!
+callers = {
+	"+14693283305": "Tabeth",
+}
+
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
 	"""Respond to incoming calls..."""
 
+	from_number = request.values.get('From', None)
+	if from_number in callers:
+		message = callers[from_number] + ", thanks for the message!"
+	else:
+		message = "Monkey, thanks for the message!"
+
 	resp = twilio.twiml.Response()
-	resp.message("Hello, Mobile Monkey")
+	with resp.message("Hello, Mobile Monkey") as m:
+		m.media("https://demo.twilio.com/own.png")
 	return str(resp)
 
 if __name__ == "__main__":
