ea231aed80122871bd4fcd395830413857faadff
Tabeth Nkangoh <benink@gmail.com>
2014-03-21 20:00:33 -0400

    init

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, session
 import twilio.twiml
 import valentine
+from random import randint
 
 SECRET_KEY = 'pineapple'
 app = Flask(__name__)
@@ -11,10 +12,32 @@ breakfast = ''
 lunch = ''
 dinner = ''
 
+#Greeting List
+greeting = {
+	'Ahoy.',
+	'Good day.',
+	'Greetings for the day.',
+	'Hello.',
+	'Hello there.',
+	'Hey.',
+	'Hi.',
+	'Hi there.',
+	'How are you? I hope you are having a good day!',
+	'How are you doing? Hope everything is good!',
+	"What's up doc?",
+	"Wazzzup.",
+	"What's popping?",
+	"Welcome.",
+	"Yo!",
+	"Sup!",
+}
+
+todayGreeting = greeting[randint(0, len(greeting))]
+
 for count in range(10):
-	breakfast += valentine.menu('Breakfast', count)
-	lunch += valentine.menu('Lunch', count)
-	dinner += valentine.menu('Dinner', count)
+	breakfast += + " " + valentine.menu('Breakfast', count)
+	lunch += " " + valentine.menu('Lunch', count)
+	dinner += " " + valentine.menu('Dinner', count)
 
 
 # Add our own number to this list!
@@ -31,18 +54,18 @@ def hello_monkey():
 
 	#First message
 	from_number = request.values.get('From')
-	message = "Thanks for the message! Do you want to know today's menu? Let me know! You can say: breakfast, lunchor dinner. By the way, you can also just say breakfast, lunch or dinner and I'll immediately text you the specified menu!"
+	message = todayGreeting + " Do you want to know today's menu? Let me know! You can say: breakfast, lunch or dinner. By the way, you can also just say breakfast, lunch or dinner and I'll immediately text you the specified menu!"
 		
 
 	#Second message
 	message_body = request.values.get('Body')
-	if message_body == 'breakfast':
+	if message_body.lower() == 'breakfast':
 		message = "Here's the breakfast menu: " + breakfast
 
-	elif message_body == 'lunch':
+	elif message_body.lower() == 'lunch':
 		message = "Here's the lunch menu: " + lunch
 
-	elif message_body == 'dinner':
+	elif message_body.lower() == 'dinner':
 		message = "Here's the dinner menu: " + dinner
 
 	resp = twilio.twiml.Response()
